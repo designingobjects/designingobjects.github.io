a6eae5f27c4bc103adaab6e34e88f58ab2ade5b5
Jerome <designingobjects@gmail.com>
2021-05-06 20:12:17 -0700

    Update mc_collectcode_100025152.js

--- mc_collectcode_100025152.js
+++ mc_collectcode_100025152.js
@@ -1,5 +1,17 @@
- function trackBase() {
-     //_etmc.push(["setOrgId", "100025152"]); // demo
-     _etmc.push(["setOrgId", "500008576"]); // test
-     _etmc.push(["trackPageView"]);
+function trackBase() {
+    _etmc.push(["setOrgId", "100025152"]);
+    _etmc.push(["trackPageView"]);
+}
+
+function trackUserInfo(userInfo) {
+    _etmc.push(["setOrgId", "100025152"]);
+    _etmc.push(["setUserInfo", {"email": userInfo}]);
+    _etmc.push(["trackPageView"]);
+}
+
+function trackBaseCleanURL() {
+    _etmc.push(["setOrgId", "100025152"]);
+    var thisURL = window.location.origin + window.location.pathname;
+    thisURL = thisURL.slice(0, -1);    
+    _etmc.push(["trackPageView", {"item" : thisURL}]);
 }
